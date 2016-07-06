3064afa7ebf96007fe8777d5c25b57956d551334
Ivan <kagemasan@gmail.com>
2016-07-06 10:40:12 +0300

    All works and it\'s fine

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     gulpif = require('gulp-if'),
     rename = require('gulp-rename'),
+    imagemin = require('gulp-imagemin'),
+    pngcrush = require('imagemin-pngcrush'),
     chalk = require('chalk');
 
 var env,
@@ -26,9 +28,11 @@ var env,
   sassSrc,
   sassStyle,
   jsonSrc,
+  imagesSrc,
   outputDir;
 
-env = process.env.NODE_ENV || 'production';
+env = process.env.NODE_ENV || 'development';
+// env = process.env.NODE_ENV || 'production';
 
 if (env === 'development') {
   outputDir = 'builds/development/';
@@ -58,7 +62,11 @@ jadeSrc = [
 ];
 
 jsonSrc = [
-    'components/js/*.json'
+    'builds/development/js/*.json'
+];
+
+imagesSrc = [
+  'builds/development/images/*.{jpg,jpeg,svg,png}'
 ];
 
 gulp.task('connect', function () {
@@ -66,6 +74,17 @@ gulp.task('connect', function () {
     root: outputDir,
     livereload: true
   });
+});
+
+gulp.task('images', function () {
+  gulp.src(imagesSrc)
+    .pipe(gulpif(env === 'production', imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngcrush()]
+    })))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+    .pipe(connect.reload())
 });
 
 gulp.task('coffee', function() {
@@ -95,7 +114,8 @@ gulp.task('jade', function () {
   gulp.src(jadeSrc)
     .pipe(pug({
       pretty: jadePretty
-    }).on('error', gutil.log))
+    })
+      .on('error', gutil.log))
     .pipe(gulp.dest(outputDir))
     .pipe(connect.reload())
 });
@@ -107,7 +127,7 @@ gulp.task('sass', function() {
       image: outputDir + 'images',
       style: sassStyle
     }))
-    .on('error', gutil.log)
+      .on('error', gutil.log)
     .pipe(gulp.dest( outputDir + 'css' ))
     .pipe(connect.reload())
 });
@@ -118,6 +138,7 @@ gulp.task('watch', function() {
   gulp.watch(jsSrc, ['js']);
   gulp.watch(sassSrc, ['sass']);
   gulp.watch(jsonSrc, ['json']);
+  gulp.watch(imagesSrc, ['images']);
 });
 
 
@@ -126,6 +147,6 @@ gulp.task('clean', function () {
 });
 
 
-gulp.task('default', ['clean', 'jade', 'json', 'coffee', 'js', 'sass', 'connect', 'watch']);
+gulp.task('default', ['clean', 'jade', 'json', 'coffee', 'js', 'sass', 'images', 'connect', 'watch']);
 
 
